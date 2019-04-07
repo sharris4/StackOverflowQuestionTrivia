cc5920b7215f161b65782bf7d1f2dff013e49ded
Sam Harris <sharris@practicevelocity.com>
2019-04-07 17:28:26 -0500

    Mostly UI changes

--- src/app/answer-list/answer-list.component.ts
+++ src/app/answer-list/answer-list.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/services/data.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Answer } from 'src/models/answer.model';
 import { ApiResponse } from 'src/models/api.response.model';
+import { MatDialog } from '@angular/material';
+import { CorrectAnswerFeedbackComponent } from 'src/app/answer-feedback/correct-answer-feedback.component';
+import { IncorrectAnswerFeedbackComponent } from '../answer-feedback/incorrect-answer-feedback.component';
+
 
 @Component({
   selector: 'app-answer-list',
@@ -13,8 +17,10 @@ export class AnswerListComponent implements OnInit {
   apiResponse: ApiResponse;
   answers: Answer[];
   callsLeft: number;
+  router: Router;
+  isCorrect: any = true;
 
-  constructor(private dataService: DataService, private route: ActivatedRoute) {
+  constructor(private dataService: DataService, private route: ActivatedRoute, public dialog: MatDialog) {
     console.log(this.route.snapshot.paramMap.get('question_id'));
   }
 
@@ -29,5 +35,27 @@ export class AnswerListComponent implements OnInit {
   ngOnInit() {
     let id = +this.route.snapshot.paramMap.get('id');
     this.getAnswerList(id);
+  }
+
+  openDialog(isAccepted: boolean) {
+    let dialogRef;
+    if (isAccepted) {
+      dialogRef = this.dialog.open(CorrectAnswerFeedbackComponent, {
+        data: { isCorrect: this.isCorrect }
+      });
+    } else {
+      dialogRef = this.dialog.open(IncorrectAnswerFeedbackComponent, {
+        data: { isCorrect: this.isCorrect }
+      });
+    }
+
+    // route back to question list after close
+    // use for score keeping?
+    dialogRef.afterClosed().subscribe(result => {
+      this.isCorrect = result;
+      this.router.navigate(['.']);
+      console.log('The dialog was closed: ' + result);
+    }
+    );
   }
 }
