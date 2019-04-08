c8e51af92fcd9e837ea4f510e19d95fcba9f0f89
Sam Harris <sharris@practicevelocity.com>
2019-04-07 19:05:44 -0500

    Fixed deploy?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {
+  MatAutocompleteModule,
+  MatBadgeModule,
+  MatBottomSheetModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDialogRef,
+  MatDividerModule,
+  MatExpansionModule,
+  MatFormFieldModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule,
+  MAT_DIALOG_DATA,
+} from '@angular/material';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +51,8 @@ import { QuestionListComponent } from './question-list/question-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { DataService } from 'src/services/data.service';
 import { AnswerListComponent } from './answer-list/answer-list.component';
+import { CorrectAnswerFeedbackComponent } from './answer-feedback/correct-answer-feedback.component';
+import { IncorrectAnswerFeedbackComponent } from './answer-feedback/incorrect-answer-feedback.component';
 import { QuestionDetailComponent } from './question-detail/question-detail.component';
 
 @NgModule({
@@ -15,19 +60,36 @@ import { QuestionDetailComponent } from './question-detail/question-detail.compo
     AppComponent,
     QuestionListComponent,
     QuestionDetailComponent,
-    AnswerListComponent
+    AnswerListComponent,
+    CorrectAnswerFeedbackComponent,
+    IncorrectAnswerFeedbackComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
+    MatCardModule,
+    MatListModule,
+    MatButtonModule,
+    MatChipsModule,
+    MatToolbarModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatProgressSpinnerModule,
+    MatInputModule,
     RouterModule.forRoot([
       { path: '', component: QuestionListComponent },
       { path: 'questions/:id', component: QuestionDetailComponent },
       { path: '', redirectTo: 'question-list', pathMatch: 'full' }
     ])
   ],
-  providers: [DataService],
-  bootstrap: [AppComponent]
+  providers: [DataService,
+    { provide: MAT_DIALOG_DATA, useValue: {} },
+    { provide: MatDialogRef, useValue: {} }],
+  bootstrap: [AppComponent],
+  entryComponents: [CorrectAnswerFeedbackComponent, IncorrectAnswerFeedbackComponent]
 })
 export class AppModule { }
+platformBrowserDynamic().bootstrapModule(AppModule);
--- src/models/question.model.ts
+++ src/models/question.model.ts
@@ -2,6 +2,7 @@ import { User } from './user.model';
 
 export class Question {
     question_id: number;
+    accepted_answer_id: number;
     answer_count: number;
     title: string;
     owner: User;
